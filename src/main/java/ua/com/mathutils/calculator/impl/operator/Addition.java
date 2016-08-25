76f5f30c5b2fb5341da2d62b3ad13bf704e4bc12
maksym.skulinets <maksym.skulinets@gmail.com>
2016-08-25 06:35:19 +0300

    Addition functionality: addition class was added.

---
+++ src/main/java/ua/com/mathutils/calculator/impl/operator/Addition.java
@@ -0,0 +1,26 @@
+package ua.com.mathutils.calculator.impl.operator;
+
+import org.apache.log4j.Logger;
+
+class Addition extends BinaryOperator {
+
+    private Logger log = Logger.getLogger(Addition.class);
+
+    Addition() {
+        super(Priority.LOW);
+    }
+
+    @Override
+    public char getRepresentation() {
+        return '+';
+    }
+
+    @Override
+    public Double execute(Double leftOperand, Double rightOperand) {
+        final double result = leftOperand + rightOperand;
+        if (log.isDebugEnabled()) {
+            log.debug(leftOperand + " + " + rightOperand + "= " + result);
+        }
+        return result;
+    }
+}
