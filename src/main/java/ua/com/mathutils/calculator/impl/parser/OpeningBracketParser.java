5148e9b1fcd4415cfa409b4644731ff95e73cada
maksym.skulinets <maksym.skulinets@gmail.com>
2016-08-26 20:39:23 +0300

    Brackets functionality:  opening bracket parser was implemented.

---
+++ src/main/java/ua/com/mathutils/calculator/impl/parser/OpeningBracketParser.java
@@ -0,0 +1,45 @@
+package ua.com.mathutils.calculator.impl.parser;
+
+import org.apache.log4j.Logger;
+import ua.com.mathutils.calculator.impl.context.InputContext;
+import ua.com.mathutils.calculator.impl.context.OutputContext;
+
+import java.util.Optional;
+
+/**
+ * Opening bracket parser.
+ */
+class OpeningBracketParser implements Parser {
+
+    private final Logger log = Logger.getLogger(OpeningBracketParser.class);
+
+    @Override
+    public Optional<EvaluationCommand> parse(InputContext input, OutputContext output) {
+
+        if (input.hasMoreToParse() && isOpeningBracket(input.getCurrentChar())) {
+
+            if (log.isDebugEnabled()) {
+                log.debug("Opening bracket parser is chosen.");
+            }
+
+            return Optional.of(new EvaluationCommand() {
+
+                @Override
+                public void execute(InputContext input, OutputContext output) {
+                    if (log.isDebugEnabled()) {
+                        log.debug("Bracket : " + input.getCurrentChar() + " is parsed.");
+                    }
+                    input.movePointer(1);
+                    output.openContext();
+                }
+            });
+        }
+
+        return Optional.empty();
+    }
+
+
+    private boolean isOpeningBracket(Character currentChar) {
+        return (currentChar == '(');
+    }
+}
