21415861eeb5b1e5793648f5f004fbf9e900eb71
maksym.skulinets <maksym.skulinets@gmail.com>
2016-09-01 01:18:29 +0300

    Minimum function functionality: minimum class interface was added.

---
+++ src/main/java/ua/com/mathutils/calculator/impl/function/Minimum.java
@@ -0,0 +1,27 @@
+package ua.com.mathutils.calculator.impl.function;
+
+import org.apache.log4j.Logger;
+
+class Minimum implements Function {
+
+    private Logger log = Logger.getLogger(Minimum.class);
+
+    @Override
+    public int getMinimumQuantityOfArguments() {
+        return 1;
+    }
+
+    @Override
+    public int getMaximumQuantityOfArguments() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public Double execute(Double leftArgument, Double rightArgument) {
+        final double result = Math.min(leftArgument, rightArgument);
+        if (log.isDebugEnabled()) {
+            log.debug("Math.min(" + leftArgument + ";" + rightArgument + ")= " + result);
+        }
+        return result;
+    }
+}
